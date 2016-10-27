8988c5d20ccdcdfef04260ba3e46de1226c43bb0
Piret Spitsõn <sppiret@gmail.com>
2016-10-27 16:44:51 +0300

    Projekti JavaFX-i variant

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main  extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vbox = new VBox();
+        Scene esimeneAken = new Scene(vbox, 500, 400);
+        primaryStage.setScene(esimeneAken);
+        primaryStage.show();
+
+        Label pealkiri = new Label("Sisesta tekst, mille raskusastet soovid teada");
+        TextField tekstiKast = new TextField();
+        Button submitbutton = new Button("Arvuta");
+
+        vbox.getChildren().addAll(pealkiri, tekstiKast, submitbutton);
+
+    }
+}
